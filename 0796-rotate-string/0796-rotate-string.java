23ac11ca9e20e412f99b3d1991a3cfe1729e30a3
HIMANSHU CHAUBEY <chaubeyhimanshu249@gmail.com>
2026-08-08 00:58:17 +0530

    Time: 2 ms (51.63%), Space: 43.5 MB (39.88%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,19 +1,71 @@
 class Solution {
+
     public boolean rotateString(String s, String goal) {
 
         if (s.length() != goal.length()) {
             return false;
         }
 
-        for (int i = 0; i < s.length(); i++) {
+        String text = s + s;
 
-            if (s.equals(goal)) {
-                return true;
+        return KMP(text, goal);
+    }
+
+    private boolean KMP(String text, String pattern) {
+
+        int[] lps = buildLPS(pattern);
+
+        int i = 0; // text index
+        int j = 0; // pattern index
+
+        while (i < text.length()) {
+
+            if (text.charAt(i) == pattern.charAt(j)) {
+                i++;
+                j++;
+
+                if (j == pattern.length()) {
+                    return true;
+                }
+            } else {
+
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
             }
-
-            s = s.substring(1) + s.charAt(0);
         }
 
         return false;
+    }
+
+    private int[] buildLPS(String pattern) {
+
+        int[] lps = new int[pattern.length()];
+
+        int len = 0;
+        int i = 1;
+
+        while (i < pattern.length()) {
+
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+
+                len++;
+                lps[i] = len;
+                i++;
+
+            } else {
+
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        return lps;
     }
 }
